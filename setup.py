06367b759560636715a6aed8e93988664dd0023c
Tyler M Kontra <tyler@tylerkontra.com>
2021-10-06 21:53:09 -0700

    chore(release): docs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="fastapi-static-digest",
-    version="0.1.0a1",
+    version="0.1.0a2",
     author="Tyler M Kontra",
     author_email="tyler@tylerkontra.com",
     description="Static file digest cache busting for FastAPI/Starlette",
